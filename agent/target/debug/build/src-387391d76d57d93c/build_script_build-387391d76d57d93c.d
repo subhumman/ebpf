0d3c2e382f01af3dfdffe05f650f4f20ea11c643
Den <denis02012000@hotmail.com>
2026-02-28 00:43:59 +0300

    /

---
+++ agent/target/debug/build/src-387391d76d57d93c/build_script_build-387391d76d57d93c.d
@@ -0,0 +1,5 @@
+C:\Users\sersa\OneDrive\Рабочий стол\pet\agent\target\debug\build\src-387391d76d57d93c\build_script_build-387391d76d57d93c.exe: build.rs
+
+C:\Users\sersa\OneDrive\Рабочий стол\pet\agent\target\debug\build\src-387391d76d57d93c\build_script_build-387391d76d57d93c.d: build.rs
+
+build.rs:
